c550461dbcc1e03ab729835908e91e2e950462b4
Iegor Stetsiura <biropka@gmail.com>
2018-12-10 14:12:51 +0200

    task_7 has done

--- task_7/script.js
+++ task_7/script.js
@@ -1,10 +1,46 @@
-console.log(getNumber(100));
+console.log('answer: ' + getResult(10001)); //result=104743
 
-function getNumber(number) {
+function getResult(number) {
 
+    var primes = [false, false],
+        result = checkPrimes(number, primes),
+        primesLimit = number;
 
+    while(!result) {
+        primesLimit *= 10;
+        primes = getEratosthen(primesLimit, primes);
+        result = checkPrimes(number, primes);
+    }
+
+    return result;
 
 }
 
+function checkPrimes(number, array) {
+    var result = 0;
+    for(var i = 2; i < array.length; i++) {
+        if(array[i]) {
+            result++;
+            if(result === number) {
+                return i;
+            }
+        }
+    }
+    return false;
+}
+
+function getEratosthen(number, array) {
+    for(var i = 2; i <= number; i++) {
+        if(array[i] === false) {
+            continue;
+        } else {
+            array[i] = true;
+        }
+        for(var j = i*2; j<= number; j+=i) {
+            array[j] = false;
+        }
+    }
+    return array;
+}
 
 
